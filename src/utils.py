59d5b3209e3747904d24212ded20d68ab95aa851
Cyprien Dujardin <103560167+CyprienDujardin@users.noreply.github.com>
2025-04-09 11:05:53 +0200

    Add files via upload

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+# maybe def AED():
+
+def preprocessing():
+    # TODO: Implement preprocessing
+    return
+
+def evaluation(model, model_name, df_train,y_train, score, *kwargs):
+    # TODO: Implement evaluation
+    return
+
+def classification():
+    # TODO: Implement classification
+    return
